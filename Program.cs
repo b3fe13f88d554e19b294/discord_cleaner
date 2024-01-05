8a3b6c6f893ee25a1c310f55a33188f8ebb6af5d
b3fe13f88d554e19b294 <951d33bac2654b9793393197fec184d3@proton.me>
2024-01-05 04:27:00 +0100

    - Created getLatestVersion() for readability - Removed useless variables

--- Program.cs
+++ Program.cs
@@ -8,55 +8,55 @@ using System.Text.RegularExpressions;
 class Program {
     static Regex appChecker = new Regex(@"^app-\d\.\d\.\d+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
-    static string[] flavors = new string[]{ 
+    static string[] defaultFlavors = new string[]{ 
         "Discord", "DiscordPTB", "DiscordCanary"
     };
 
-    static void fixFlavor(string flavor="Discord") {
-        string path = Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), flavor);
-        Console.WriteLine(path);
-        if(Directory.Exists(path)) {
-            IDictionary<int, string> vers = new Dictionary<int, string>();
-            foreach (var file in Directory.GetDirectories(path, "app*")){
-                string folderName = Path.GetFileName(file);
-                MatchCollection match = appChecker.Matches(folderName);
-                if(!(match.Count > 0)) {
-                    // Not an app.x.x.x+ folder
-                    continue;
-                }
-
-                string versionString = folderName.Split("-")[1];
-                string[] vComp = versionString.Split(".");
-                versionString = "";
-                foreach (var item in vComp){
-                    int vnum;
-                    int.TryParse(item, out vnum);
-                    versionString += vnum.ToString();
-                }
-                int version;
-                int.TryParse(versionString, out version);
-                vers.Add(version, file);
-            }
-            string finalPath = vers[vers.Keys.Max()];
-
-            string jsPath = Path.Combine(Directory.GetDirectories(Path.Combine(finalPath, "modules"), "discord_desktop_core-*")[0], "discord_desktop_core", "index.js");
+    static string getLatestVersion(string path) {
+        IDictionary<double, string> versions = new Dictionary<double, string>();
+        foreach (var file in Directory.GetDirectories(path, "app*")){
+            string folderName = Path.GetFileName(file);
             
-            int lineCount = File.ReadLines(jsPath).Count();
+            MatchCollection match = appChecker.Matches(folderName);
+            if (match.Count() < 1) continue;
             
-
-            string finalString = File.ReadAllText(jsPath);
-            if (lineCount > 1) {
-                Regex r = new Regex(@"module\.exports = require\(.*\);(.*)", RegexOptions.Singleline);
-                string quote = "\"";
-                finalString = r.Replace(finalString, @"module.exports = require("+ quote +"./core.asar"+ quote +");");
-            }
-            File.WriteAllText(jsPath, finalString);
+            string[] n = folderName.Split("-")[1].Split(".");
+            string versionString = n[0].ToString() + n[1].ToString() + "." + n[2].ToString();
+            double.TryParse(versionString, out double version);
+            versions.Add(version, file);
         }
+        return versions[versions.Keys.Max()];
     }
 
-    static void Main(string[] args) {
-        foreach (var flavor in flavors){
+    static void fixFlavor(string flavor="Discord") {
+        string path = Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), flavor);        
+        if(!Directory.Exists(path)) return;
+        
+        string vers = getLatestVersion(path);
+        if(vers.Length == 0) return;
+
+        string jsPath = Path.Combine(Directory.GetDirectories(Path.Combine(vers, "modules"), "discord_desktop_core-*")[0], "discord_desktop_core", "index.js");
+        string finalString = File.ReadAllText(jsPath);
+        
+        if (File.ReadLines(jsPath).Count() > 1) {
+            Regex r = new Regex(@"module\.exports = require\(.*\);(.*)", RegexOptions.Singleline);
+            string quote = "\"";
+            finalString = r.Replace(finalString, @"module.exports = require("+ quote +"./core.asar"+ quote +");");
+        }
+
+        File.WriteAllText(jsPath, finalString);
+    }
+
+    static void Main(string[] args) { // discord_cleaner.exe <--PTB|--Canary|--Development>
+        if (args.Length > 1) {
+            foreach (var farg in args) {
+                string arg = farg[2..];
+                fixFlavor("Discord"+arg);
+            }
+        } else {
+            foreach (var flavor in defaultFlavors){
             fixFlavor(flavor);
+        }
         }
     }
 }
