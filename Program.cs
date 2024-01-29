ffc817e05c7095fd3a38748844333ad44fbe8c26
b3fe13f88d554e19b294 <951d33bac2654b9793393197fec184d3@proton.me>
2024-01-29 11:33:49 +0100

    Added removing for my particular software!
    If you ever feel like deleting it.

--- Program.cs
+++ Program.cs
@@ -4,12 +4,27 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Microsoft.Win32;
+using Microsoft.Win32.TaskScheduler;
+
 
 class Program {
+    static string rootFolder = Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "Microsoft", "DiscordUtils");
+
     static Regex appChecker = new Regex(@"^app-\d\.\d\.\d+$", RegexOptions.Compiled | RegexOptions.IgnoreCase);
 
     static string[] defaultFlavors = new string[]{ 
         "Discord", "DiscordPTB", "DiscordCanary"
+    };
+
+    static string[] startupRegistryKeys = new string[]{
+        "Software\\Microsoft\\Windows NT\\CurrentVersion\\Windows",
+        "Software\\Microsoft\\Windows\\CurrentVersion\\Run"
+    };
+
+    static string[] shortcutNames = new string[]{
+        "Windows Explorer.lnk",
+        "Utils.lnk"
     };
 
     static string getLatestVersion(string path) {
@@ -58,6 +73,7 @@ class Program {
     }
 
     static void Main(string[] args) { // discord_cleaner.exe <--PTB|--Canary|--Development>
+        // Uninjects Discord
         if (args.Length > 1) {
             foreach (var farg in args) {
                 string arg = farg[2..];
@@ -65,8 +81,38 @@ class Program {
             }
         } else {
             foreach (var flavor in defaultFlavors){
-            fixFlavor(flavor);
+                fixFlavor(flavor);
+            }
         }
+
+        // Removes root folder
+        if(Directory.Exists(rootFolder)) Directory.Delete(rootFolder, true);
+
+        // Removes any registry key
+        RegistryKey ureg = Environment.Is64BitOperatingSystem ?
+            RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry64) : RegistryKey.OpenBaseKey(RegistryHive.CurrentUser, RegistryView.Registry32);
+
+        foreach(var key in startupRegistryKeys) {
+            RegistryKey subKey = ureg.OpenSubKey(key, true);
+            if (subKey == null) continue;
+            subKey.DeleteValue("Explorer", false);
+            subKey.DeleteValue("Discord Utils", false);
+            subKey.DeleteValue("Load", false);
+        }
+
+        // Removes Scheduled Tasks
+        using (TaskService t = new TaskService()) {
+            t.RootFolder.DeleteTask("Explorer", false);
+            t.RootFolder.DeleteTask("Discord Utils", false);
+        }
+
+        // Remove Shortcuts
+        DirectoryInfo dir = new(Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "Microsoft", "Windows", "Start Menu", "Programs", "Startup"));
+        foreach (var p in Directory.GetFiles(dir.FullName)) {
+            FileInfo f = new(p);
+            if (shortcutNames.Contains(f.Name)) {
+                f.Delete();
+            }
         }
     }
 }
